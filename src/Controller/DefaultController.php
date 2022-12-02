fde5841c2195b1def470b91361271a4f2eecbb39
B-Forest <benjamin.frt28@gmail.com>
2022-12-02 09:14:07 +0100

    récuperer les instruments en base de données

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\InstrumentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +10,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(InstrumentRepository $instrumentRepository): Response
     {
+        $instrument = $instrumentRepository->findAll();
+        dump($instrument);
         return $this->render('default/homepage.html.twig');
     }
 }
