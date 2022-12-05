bef66da72a64c973760b0054c17cd550c46a9995
B-Forest <benjamin.frt28@gmail.com>
2022-12-05 16:31:35 +0100

    affichage de la liste des musiciens sur une page dédié

---
+++ src/Controller/MusicianController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\MusicianRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MusicianController extends AbstractController
+{
+    #[Route('/musician', name: 'musician_list')]
+    public function list(MusicianRepository $musiciansRepository): Response
+    {
+
+        return $this->render('musician/index.html.twig', [
+            'musicians' => $musiciansRepository->findAll(),
+        ]);
+    }
+}
