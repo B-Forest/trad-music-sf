ba216ae70450d3e6df1757fb0784fe95b48d245d
B-Forest <benjamin.frt28@gmail.com>
2022-12-05 11:01:37 +0100

    Ajout des fixtures User Manager Musician Participant

--- src/DataFixtures/PubFixtures.php
+++ src/DataFixtures/PubFixtures.php
@@ -4,9 +4,10 @@ namespace App\DataFixtures;
 
 use App\Entity\Pub;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class PubFixtures extends Fixture
+class PubFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
@@ -16,30 +17,39 @@ class PubFixtures extends Fixture
         $oconnells->setAddress("6 Pl. du Parlement de Bretagne");
         $oconnells->setZipCode("35000");
         $oconnells->setCity("Rennes");
+        $oconnells->setManager($this->getReference('manager-oconnells'));
         $manager->persist($oconnells);
-        $this->addReference('pub-oconnells', $oconnells);
+        $this->addReference("pub-oconnells", $oconnells);
 
         $templeBar = new Pub();
         $templeBar->setName("Temple Bar");
         $templeBar->setImage("templebar.jpg");
-        $templeBar->setAddress("647-48 Temple Bar");
+        $templeBar->setAddress("47-48 Temple Bar");
         $templeBar->setZipCode("D02 N725");
         $templeBar->setCity("Dublin");
+        $templeBar->setManager($this->getReference('manager-templebar'));
         $manager->persist($templeBar);
-        $this->addReference('pub-templebar', $templeBar);
+        $this->addReference("pub-templebar", $templeBar);
 
         $theBrazenHead = new Pub();
         $theBrazenHead->setName("The Brazen Head");
         $theBrazenHead->setImage("thebrazenhead.jpg");
+        $theBrazenHead->setManager($this->getReference('manager-thebrazenhead'));
         $manager->persist($theBrazenHead);
-        $this->addReference('pub-thebrazenhead', $theBrazenHead);
+        $this->addReference("pub-thebrazenhead", $theBrazenHead);
 
         $mulligans = new Pub();
-        $mulligans->setName("Mulligans");
+        $mulligans->setName("Mulligan's");
         $mulligans->setImage("mulligans.jpg");
+        $mulligans->setManager($this->getReference('manager-mulligans'));
         $manager->persist($mulligans);
-        $this->addReference('pub-mulligans', $mulligans);
+        $this->addReference("pub-mulligans", $mulligans);
 
         $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [ManagerFixtures::class];
     }
 }
