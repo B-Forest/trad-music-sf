1a9b2469c85568bb12b23922bbcb20b1f943c4a6
B-Forest <benjamin.frt28@gmail.com>
2022-12-05 09:03:42 +0100

    Création fixtures Gig

--- src/DataFixtures/PubFixtures.php
+++ src/DataFixtures/PubFixtures.php
@@ -10,13 +10,14 @@ class PubFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $oconnels = new Pub();
-        $oconnels->setName("O'Connell's");
-        $oconnels->setImage("oconnell.jpg");
-        $oconnels->setAddress("6 Pl. du Parlement de Bretagne");
-        $oconnels->setZipCode("35000");
-        $oconnels->setCity("Rennes");
-        $manager->persist($oconnels);
+        $oconnells = new Pub();
+        $oconnells->setName("O'Connell's");
+        $oconnells->setImage("oconnell.jpg");
+        $oconnells->setAddress("6 Pl. du Parlement de Bretagne");
+        $oconnells->setZipCode("35000");
+        $oconnells->setCity("Rennes");
+        $manager->persist($oconnells);
+        $this->addReference('pub-oconnells', $oconnells);
 
         $templeBar = new Pub();
         $templeBar->setName("Temple Bar");
@@ -25,16 +26,19 @@ class PubFixtures extends Fixture
         $templeBar->setZipCode("D02 N725");
         $templeBar->setCity("Dublin");
         $manager->persist($templeBar);
+        $this->addReference('pub-templebar', $templeBar);
 
         $theBrazenHead = new Pub();
         $theBrazenHead->setName("The Brazen Head");
         $theBrazenHead->setImage("thebrazenhead.jpg");
         $manager->persist($theBrazenHead);
+        $this->addReference('pub-thebrazenhead', $theBrazenHead);
 
         $mulligans = new Pub();
         $mulligans->setName("Mulligans");
         $mulligans->setImage("mulligans.jpg");
         $manager->persist($mulligans);
+        $this->addReference('pub-mulligans', $mulligans);
 
         $manager->flush();
     }
