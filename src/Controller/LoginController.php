b04c5730f2d07390e3e7d1e7dab5e8d4d4b91615
B-Forest <benjamin.frt28@gmail.com>
2022-12-07 11:56:10 +0100

    Création page de login et controller

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends AbstractController
+{
+    #[Route('/login', name: 'login')]
+    public function index(AuthenticationUtils $authenticationUtils): Response
+    {
+        // Récuperer les erreurs du formulaire si il y en a
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // Récuperer le dernier email saisi
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('login/index.html.twig', [
+            'error' => $error,
+            'last_username' => $lastUsername
+
+        ]);
+    }
+}
